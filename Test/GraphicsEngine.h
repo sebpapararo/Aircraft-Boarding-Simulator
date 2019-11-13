658cdd314df0671fb528626825ec23767272e72f
Sebastian Papararo <s.papararo@gmail.com>
2019-11-13 16:05:13 +0000

    Tidy up this file

--- Test/GraphicsEngine.h
+++ Test/GraphicsEngine.h
@@ -7,16 +7,7 @@
 #include "PhysicsEngine.h"
 #include "SeatingStrategies.h"
 
-#include <GL\GL.h>			// Header File For The OpenGL32 Library
-#include <GL\GLU.h>			// Header File For The GLu32 Library
-#include <glut/glut.h>		// GLUT, includes glu.h and gl.h
-
 #include <string>
-#include <fstream>
-
-// #include <iostream>
-
-// #include "GLFW/glfw3.h"
 
 class GraphicsEngine {
 public:
@@ -26,7 +17,6 @@ private:
 	static void init();
 	static void display();
 	static void FPSLock();
-	//static void displayFPS();
 	static void infoDisplay(std::string outputString, float posX, float posY);
 	static void processKeys(unsigned char key, int x, int y);
 	static void reshape(int width, int height);

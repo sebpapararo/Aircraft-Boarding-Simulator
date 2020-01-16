2b44e9e76ec2a239f89f73449b52462cccb9bad0
qke16pwu <qke16pwu@CMPLAB1-16>
2020-01-16 10:57:09 +0000

    gf

--- Test/ConsoleEngine.cpp
+++ Test/ConsoleEngine.cpp
@@ -140,7 +140,7 @@ void ConsoleEngine::runConsoleEngine() {
 	auto endTime = std::chrono::high_resolution_clock::now();
 	std::cout << "\nAll passengers are seated." << std::endl;
 	std::cout << "The algorithm used was: '" << c_currentAlgorithm << "' and the plane template was: '" << c_aircraftName << "'" << std::endl;
-    std::cout << "Total number of seats/passengers was: " + totalPassengers << std::endl;
+    std::cout << "Total number of seats/passengers was: " << totalPassengers << std::endl;
 	std::cout << "Everyone is seated. The total runtime was (in seconds): " << std::chrono::duration<double>(endTime - startTime).count() * c_PE.getSimSpeed() << std::endl;
 	std::cout << "Everyone is seated. Average time per passenger to be seated was (in seconds): " << c_PE.getAverageSeatedTime() * c_PE.getSimSpeed() << std::endl;
 	system("pause");
